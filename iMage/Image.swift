9aee06e49863897978c3c005ee0730655968550c
Radu Costea <raducostea@192-168-0-100.rdsnet.ro>
2017-05-09 00:44:29 +0300

    tests failing

--- iMage/Image.swift
+++ iMage/Image.swift
@@ -37,3 +37,4 @@ class Image: ImageEntry {
         self.imgImage = imgImage
     }
 }
+
--- iMageTests/ImageEntryCellTests.swift
+++ iMageTests/ImageEntryCellTests.swift
@@ -7,13 +7,28 @@
 //
 
 import XCTest
-import FBSnapshotTestCase
+@testable import iMage
+import FBSnapshotTestCase.Swift
+
+class ImageMock: ImageEntry {
+    var caption: String { return "This image is gorgeous" }
+    var formattedDate: String { return DateFormatter.localizedString(from: Date.distantPast, dateStyle: .medium, timeStyle: .short) }
+    var imageTask: AsyncTask<UIImage> {
+        let task = AsyncTask<UIImage>()
+//        let url = Bundle(for: type(of: self)).url(forResource: "imageTest", withExtension: "jpeg")
+        let data = try! Data(contentsOf: URL(string: "https://koenig-media.raywenderlich.com/uploads/2014/11/SuperDev.jpg")!)
+        task.reportSuccess(with: UIImage(data: data)!)
+//        task.reportSuccess(with: UIImage(named: "imageTest", in: Bundle(for: type(of: self)), compatibleWith: nil)!)
+        return task
+    }
+}
 
 class ImageEntryCellTests: FBSnapshotTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        recordMode = true
     }
     
     override func tearDown() {
@@ -24,6 +39,9 @@ class ImageEntryCellTests: FBSnapshotTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let cell = UINib(nibName: "ImageEntryTableViewCell", bundle: nil).instantiate(withOwner: nil, options: nil).first! as! ImageEntryTableViewCell 
+        cell.imageEntry = ImageMock()
+        FBSnapshotVerifyView(cell, identifier: "")
     }
     
     func testPerformanceExample() {
